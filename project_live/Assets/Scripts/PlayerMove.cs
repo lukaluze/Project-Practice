aed509f11293e3d05bc940bfeaabbd03d1a04b72
SeokKyu Lee <89951843+lukaluze@users.noreply.github.com>
2023-04-10 17:00:12 +0900

    점프
    캐릭터의 점프 제작 & 애니메이션
    input받는 것을 하나로 묶음
    
    캐릭터가 점프중에 이동키와 앉기 적용안됨

--- project_live/Assets/Scripts/PlayerMove.cs
+++ project_live/Assets/Scripts/PlayerMove.cs
@@ -6,35 +6,59 @@ public class PlayerMove : MonoBehaviour
 {
     public float Dpi; //민감도
     public float speed; //이동 속도
+    public float jumpfower;//점프 힘
+
 
     [SerializeField]
     private Transform characterBody;
     [SerializeField]
     private Transform cameraArm;
-    bool wDown;
+
+    Rigidbody rigid; //스크립트 객체의 rigidbody
+
+    //입력 관련 키
+    float hPlayerAsix; // <> 
+    float vPlayerAsix; // ㅅV
+    bool jDown; //space
+    bool wDown; //left shift
+    bool isJump; //점프중?
+    float x_Mouse;
+    float y_Mouse;
 
     Animator animator;
 
     void Start()
     {
-        animator = characterBody.GetComponentInChildren<Animator>();
+        animator = characterBody.GetComponentInChildren<Animator>(); //애니메이션 불러오기
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        inputKey(); 
         LookAround();
         Move();
+        Junp();
     }
     
-    private void Move()
+    void inputKey() // 키 입력 모음
+    {
+        if (isJump == false) { //점프중 이동 방향 및 걷기 제한
+            hPlayerAsix = Input.GetAxisRaw("Horizontal");
+            vPlayerAsix = Input.GetAxisRaw("Vertical");
+            wDown = Input.GetButton("Walk"); //걷기
+        }
+        x_Mouse = Input.GetAxis("Mouse X"); //마우스 위 아래
+        y_Mouse = Input.GetAxis("Mouse Y"); //마우스 좌 우
+        jDown = Input.GetButtonDown("Jump");
+    }
+    private void Move() //캐릭터 이동
     {   
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
-        wDown = Input.GetButton("Walk"); //걷기
-
+        Vector2 moveVec= new Vector2(hPlayerAsix,vPlayerAsix);
         Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
         Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
-        Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+        Vector3 moveDir = lookForward * vPlayerAsix + lookRight * hPlayerAsix;
 
         characterBody.forward = lookForward;
 
@@ -43,29 +67,45 @@ public class PlayerMove : MonoBehaviour
         else
             transform.position += moveDir * Time.deltaTime * speed;
 
-        animator.SetBool("isRun", moveInput != Vector2.zero);
+        animator.SetBool("isRun", moveVec != Vector2.zero);
         animator.SetBool("isWalk", wDown);
-        
-        
-
-        Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+ 
+        Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red); //캐릭 보는 방향 디버그
     }
 
-    void LookAround()
+    void Junp()
     {
-        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X") * Dpi ,Input.GetAxis("Mouse Y") * Dpi);
+        if (jDown&&!isJump)
+        {
+            rigid.AddForce(Vector3.up * jumpfower,ForceMode.Impulse);
+            isJump = true;
+            animator.SetBool("isJump", true);
+            animator.SetTrigger("doJump");
+        }
+    }
+
+    void LookAround() // 카메라 회전
+    {
+        Vector2 mouseDelta = new Vector2(x_Mouse * Dpi , y_Mouse * Dpi);
         Vector3 camAngle = cameraArm.rotation.eulerAngles;
         float x = camAngle.x - mouseDelta.y;
 
-        if(x < 180f)
+        if(x < 180f) //위아래 각도
         {
-            x = Mathf.Clamp(x, -1f, 70f);
+            x = Mathf.Clamp(x, -1f, 30f);
         }else{
-            x = Mathf.Clamp(x, 335f, 361f);
+            x = Mathf.Clamp(x, 330f, 361f);
         }
 
         cameraArm.rotation = Quaternion.Euler(x,camAngle.y+mouseDelta.x,camAngle.z);
+    }
 
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor") //바닥에 닿으면
+        {
+            isJump = false;
+            animator.SetBool("isJump", false);
+        }
     }
 }
